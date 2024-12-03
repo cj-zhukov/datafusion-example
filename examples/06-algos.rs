0d276c467051f6b2401df00ef5bd792191a42d9d
Sergey Zhukov <szhukov@aligntech.com>
2024-12-03 09:36:01 +0300

    add example round-robin selection of workers

---
+++ examples/06-algos.rs
@@ -0,0 +1,45 @@
+use std::sync::Arc;
+
+use anyhow::Result;
+use datafusion::arrow::array::{Float64Array, Int32Array, RecordBatch, StringArray};
+use datafusion::arrow::datatypes::{DataType, Field, Schema};
+use datafusion::prelude::*;
+use datafusion_example::utils::utils::df_to_table;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    round_robin_example().await?;
+
+    Ok(())
+}
+
+/// Round-Robin Selection of Workers
+pub async fn round_robin_example() -> Result<()> {
+    let ctx = SessionContext::new();
+    let schema = Schema::new(vec![
+        Field::new("id", DataType::Int32, false),
+        Field::new("name", DataType::Utf8, true),
+        Field::new("data", DataType::Float64, true),
+    ]);
+    let batch = RecordBatch::try_new(
+        schema.clone().into(),
+        vec![
+            Arc::new(Int32Array::from(vec![1, 2, 3])),
+            Arc::new(StringArray::from(vec!["foo", "bar", "baz"])),
+            Arc::new(Float64Array::from(vec![1.0, 10.0, 100.0])),
+        ],
+    )?;
+    let df = ctx.read_batch(batch.clone())?;
+    let table_name = "t";
+    df_to_table(ctx.clone(), df.clone(), table_name).await?;
+
+    let mut cur_worker = 1;
+    while cur_worker <= 5 {
+        let round_robin_sql = format!("(({cur_worker} - 1) % (select count(*) from {table_name})) + 1");
+        let df = ctx.sql(&format!("select * from {table_name} where id = {round_robin_sql}")).await?;
+        df.show().await?;
+        cur_worker += 1;
+    }
+
+    Ok(())
+}
