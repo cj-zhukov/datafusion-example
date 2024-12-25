6f6f5ffb0fee301163b5bb53f3d3f5bd5106f590
Sergey Zhukov <szhukov@aligntech.com>
2024-12-25 16:02:07 +0300

    docs: add new example random_example()

--- examples/07-algos.rs
+++ examples/07-algos.rs
@@ -9,6 +9,7 @@ use datafusion_example::utils::utils::df_to_table;
 #[tokio::main]
 async fn main() -> Result<()> {
     round_robin_example().await?;
+    random_example().await?;
 
     Ok(())
 }
@@ -39,6 +40,39 @@ pub async fn round_robin_example() -> Result<()> {
         let df = ctx.sql(&format!("select * from {table_name} where id = {round_robin_sql}")).await?;
         df.show().await?;
         cur_worker += 1;
+    }
+
+    Ok(())
+}
+
+pub async fn random_example() -> Result<()> {
+    let ctx = SessionContext::new();
+    let schema = Schema::new(vec![
+        Field::new("id", DataType::Int32, false),
+        Field::new("name", DataType::Utf8, true),
+        Field::new("data", DataType::Float64, true),
+    ]);
+    let batch = RecordBatch::try_new(
+        schema.clone().into(),
+        vec![
+            Arc::new(Int32Array::from(vec![1, 2, 3])),
+            Arc::new(StringArray::from(vec!["foo", "bar", "baz"])),
+            Arc::new(Float64Array::from(vec![1.0, 10.0, 100.0])),
+        ],
+    )?;
+    let df = ctx.read_batch(batch.clone())?;
+    let table_name = "t";
+    df_to_table(ctx.clone(), df, table_name).await?;
+
+    let sql = format!("select *
+                                from {table_name} 
+                                where 1 = 1 
+                                order by random()
+                                limit 1");
+
+    for _ in 0..5 {
+        let df = ctx.sql(&sql).await?;
+        df.show().await?;
     }
 
     Ok(())
