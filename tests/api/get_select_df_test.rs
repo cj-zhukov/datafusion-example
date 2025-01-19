a207434713d93aa6ae08a524e1d6f1dbf7302934
Sergey Zhukov <szhukov@aligntech.com>
2025-01-19 09:04:18 +0300

    docs: use Result from color_eyre

--- tests/api/get_select_df_test.rs
+++ tests/api/get_select_df_test.rs
@@ -1,3 +1,4 @@
+use color_eyre::Result;
 use datafusion::assert_batches_eq;
 use datafusion::prelude::*;
 use rstest::rstest;
@@ -6,22 +7,23 @@ use datafusion_example::utils::utils::*;
 use crate::helpers::*;
 
 #[rstest]
-#[case(get_df1().unwrap(), vec!["id", "name", "data"])]
-#[case(get_df2().unwrap(), vec!["id", "name"])]
-#[case(get_df3().unwrap(), vec!["id", "data"])]
-fn test_get_column_names(#[case] df: DataFrame,#[case] expected: Vec<&str>) {
-    assert_eq!(expected, get_column_names(df))
+#[case(get_df1()?, vec!["id", "name", "data"])]
+#[case(get_df2()?, vec!["id", "name"])]
+#[case(get_df3()?, vec!["id", "data"])]
+fn test_get_column_names(#[case] df: DataFrame,#[case] expected: Vec<&str>) -> Result<()> {
+    assert_eq!(expected, get_column_names(df));
+    Ok(())
 }
 
 #[tokio::test]
-async fn test_select_all_exclude() {
-    let df = get_df1().unwrap();
-    let res = select_all_exclude(df, &["data"]).unwrap();
+async fn test_select_all_exclude() -> Result<()> {
+    let df = get_df1()?;
+    let res = select_all_exclude(df, &["data"])?;
 
     assert_eq!(res.schema().fields().len(), 2); // columns count
-    assert_eq!(res.clone().count().await.unwrap(), 3); // rows count
+    assert_eq!(res.clone().count().await?, 3); // rows count
 
-    let rows = res.sort(vec![col("id").sort(true, true)]).unwrap();
+    let rows = res.sort(vec![col("id").sort(true, true)])?;
     assert_batches_eq!(
         &[
             "+----+------+",
@@ -32,6 +34,8 @@ async fn test_select_all_exclude() {
             "| 3  | baz  |",
             "+----+------+",
         ],
-        &rows.collect().await.unwrap()
+        &rows.collect().await?
     );
+
+    Ok(())
 }
