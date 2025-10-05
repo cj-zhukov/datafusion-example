e278b702c746c57093b74bf23cfc5ee04cd8781f
Sergey Zhukov <szhukov@aligntech.com>
2025-10-05 09:48:18 +0300

    docs: deserialize example for optional columns in dataframe

--- examples/05-deserialize-to-struct.rs
+++ examples/05-deserialize-to-struct.rs
@@ -17,7 +17,7 @@ pub struct Foo {
 impl Foo {
     pub fn schema() -> Schema {
         Schema::new(vec![
-            Field::new("id", DataType::Int32, false),
+            Field::new("id", DataType::Int32, true),
             Field::new("name", DataType::Utf8, true),
             Field::new("data", DataType::Binary, true),
         ])
@@ -28,6 +28,14 @@ impl Foo {
             Arc::new(Int32Array::from(vec![1, 2, 3])),
             Arc::new(StringArray::from(vec!["foo", "bar", "baz"])),
             Arc::new(BinaryArray::from_vec(vec![b"foo", b"bar", b"baz"])),
+        ]
+    }
+
+    pub fn data_with_null() -> Vec<Arc<dyn Array>> {
+        vec![
+            Arc::new(Int32Array::from(vec![Some(1), Some(2), None])),
+            Arc::new(StringArray::from(vec![Some("foo"), None, None])),
+            Arc::new(BinaryArray::from_opt_vec(vec![Some(b"foo"), None, None])),
         ]
     }
 }
@@ -111,6 +119,63 @@ impl Foo {
         println!("{:?}", records);
         Ok(())
     }
+
+    /// shows how to deserialize dataframe to struct, if some columns don't exist in dataframe
+    pub async fn example3(ctx: &SessionContext) -> Result<()> {
+        let schema = Self::schema();
+        let data = Self::data_with_null();
+        let batch = RecordBatch::try_new(Arc::new(schema), data)?;
+        let df = ctx.read_batch(batch)?;
+
+        let mut stream = df.execute_stream().await?;
+        let mut records = vec![];
+        while let Some(batch) = stream.next().await.transpose()? {
+            let schema = batch.schema();
+            let columns = schema.fields().iter().map(|f| f.name().clone()).collect::<Vec<_>>();
+
+            // helper closure to get optional StringArray
+            let get_string_col = |name: &str| -> Option<&StringArray> {
+                columns
+                    .iter()
+                    .position(|n| n == name)
+                    .map(|i| batch.column(i).as_any().downcast_ref::<StringArray>())
+                    .flatten()
+            };
+
+            // helper closure for Int32Array
+            let get_int_col = |name: &str| -> Option<&Int32Array> {
+                columns
+                    .iter()
+                    .position(|n| n == name)
+                    .map(|i| batch.column(i).as_any().downcast_ref::<Int32Array>())
+                    .flatten()
+            };
+
+            // helper closure for BinaryArray
+            let get_bin_col = |name: &str| -> Option<&BinaryArray> {
+                columns
+                    .iter()
+                    .position(|n| n == name)
+                    .map(|i| batch.column(i).as_any().downcast_ref::<BinaryArray>())
+                    .flatten()
+            };
+
+            let ids = get_int_col("id");
+            let names = get_string_col("name");
+            let data_vals = get_bin_col("data");
+
+            for i in 0..batch.num_rows() {
+                records.push(Self {
+                    id: ids.and_then(|col| if col.is_null(i) { None } else { Some(col.value(i)) }),
+                    name: names.and_then(|col| if col.is_null(i) { None } else { Some(col.value(i).to_string()) }),
+                    data_val: data_vals.and_then(|col| if col.is_null(i) { None } else { Some(col.value(i).to_vec()) }),
+                });
+            }
+        }
+
+        println!("{:?}", records);
+        Ok(())
+    }
 }
 
 #[tokio::main]
@@ -118,5 +183,6 @@ async fn main() -> Result<()> {
     let ctx = SessionContext::new();
     Foo::example1(&ctx).await?;
     Foo::example2(&ctx).await?;
+    Foo::example3(&ctx).await?;
     Ok(())
 }
