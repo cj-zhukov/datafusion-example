49081edc91e179655c27acd800a54800b86d5d44
Sergey Zhukov <cjthebest89@gmail.com>
2026-04-07 10:14:09 +0400

    docs: use cars dataset in tests helpers

--- tests/api/helpers_tests.rs
+++ tests/api/helpers_tests.rs
@@ -4,32 +4,36 @@ use datafusion::arrow::datatypes::DataType;
 use datafusion::assert_batches_eq;
 use datafusion::prelude::*;
 
+use datafusion_example::utils::datasets::ExampleDataset;
 use datafusion_example::utils::helpers::*;
 
 #[tokio::test]
 async fn test_add_pk_to_df() -> Result<()> {
-    let df = dataframe!(
-        "id" => [1, 2, 3],
-        "name" => ["foo", "bar", "baz"],
-        "data" => [42, 43, 44]
-    )?;
-
     let ctx = SessionContext::new();
+    let cars = ExampleDataset::Cars;
+    let df = cars.dataframe(&ctx).await?;
     let res = add_pk_to_df(&ctx, df, "pk").await?;
 
     assert_eq!(res.schema().fields().len(), 4); // columns count
-    assert_eq!(res.clone().count().await?, 3); // rows count
+    assert_eq!(res.clone().count().await?, 25); // rows count
 
-    let rows = res.sort(vec![col("id").sort(true, true)])?;
+    let rows = res.sort(vec![col("pk").sort(true, true)])?.limit(0, Some(10))?;
     assert_batches_eq!(
         &[
-            "+----+------+------+----+",
-            "| id | name | data | pk |",
-            "+----+------+------+----+",
-            "| 1  | foo  | 42   | 0  |",
-            "| 2  | bar  | 43   | 1  |",
-            "| 3  | baz  | 44   | 2  |",
-            "+----+------+------+----+",
+            "+-----+-------+---------------------+----+",
+            "| car | speed | time                | pk |",
+            "+-----+-------+---------------------+----+",
+            "| red | 20.0  | 1996-04-12T12:05:03 | 0  |",
+            "| red | 20.3  | 1996-04-12T12:05:04 | 1  |",
+            "| red | 21.4  | 1996-04-12T12:05:05 | 2  |",
+            "| red | 21.5  | 1996-04-12T12:05:06 | 3  |",
+            "| red | 19.0  | 1996-04-12T12:05:07 | 4  |",
+            "| red | 18.0  | 1996-04-12T12:05:08 | 5  |",
+            "| red | 17.0  | 1996-04-12T12:05:09 | 6  |",
+            "| red | 7.0   | 1996-04-12T12:05:10 | 7  |",
+            "| red | 7.1   | 1996-04-12T12:05:11 | 8  |",
+            "| red | 7.2   | 1996-04-12T12:05:12 | 9  |",
+            "+-----+-------+---------------------+----+",
         ],
         &rows.collect().await?
     );
@@ -40,27 +44,47 @@ async fn test_add_pk_to_df() -> Result<()> {
 #[tokio::test]
 async fn test_add_int_col_to_df() -> Result<()> {
     let ctx = SessionContext::new();
-    let df = dataframe!(
-        "id" => [1, 2, 3],
-        "name" => ["foo", "bar", "baz"]
-    )?;
+    let cars = ExampleDataset::Cars;
+    let df = cars.dataframe(&ctx).await?;
 
-    let data = vec![42, 43, 44];
+    let data = (0..25).step_by(1).collect::<Vec<i32>>();
     let res = add_int_col_to_df(&ctx, df, data, "data").await?;
 
-    assert_eq!(res.schema().fields().len(), 3); // columns count
-    assert_eq!(res.clone().count().await?, 3); // rows count
+    assert_eq!(res.schema().fields().len(), 4); // columns count
+    assert_eq!(res.clone().count().await?, 25); // rows count
 
-    let rows = res.sort(vec![col("id").sort(true, true)])?;
+    let rows = res.sort(vec![col("car").sort(true, true)])?;
     assert_batches_eq!(
         &[
-            "+----+------+------+",
-            "| id | name | data |",
-            "+----+------+------+",
-            "| 1  | foo  | 42   |",
-            "| 2  | bar  | 43   |",
-            "| 3  | baz  | 44   |",
-            "+----+------+------+",
+            "+-------+-------+---------------------+------+",
+            "| car   | speed | time                | data |",
+            "+-------+-------+---------------------+------+",
+            "| green | 10.0  | 1996-04-12T12:05:03 | 13   |",
+            "| green | 10.3  | 1996-04-12T12:05:04 | 14   |",
+            "| green | 10.4  | 1996-04-12T12:05:05 | 15   |",
+            "| green | 10.5  | 1996-04-12T12:05:06 | 16   |",
+            "| green | 11.0  | 1996-04-12T12:05:07 | 17   |",
+            "| green | 12.0  | 1996-04-12T12:05:08 | 18   |",
+            "| green | 14.0  | 1996-04-12T12:05:09 | 19   |",
+            "| green | 15.0  | 1996-04-12T12:05:10 | 20   |",
+            "| green | 15.1  | 1996-04-12T12:05:11 | 21   |",
+            "| green | 15.2  | 1996-04-12T12:05:12 | 22   |",
+            "| green | 8.0   | 1996-04-12T12:05:13 | 23   |",
+            "| green | 2.0   | 1996-04-12T12:05:14 | 24   |",
+            "| red   | 20.0  | 1996-04-12T12:05:03 | 0    |",
+            "| red   | 20.3  | 1996-04-12T12:05:04 | 1    |",
+            "| red   | 21.4  | 1996-04-12T12:05:05 | 2    |",
+            "| red   | 21.5  | 1996-04-12T12:05:06 | 3    |",
+            "| red   | 19.0  | 1996-04-12T12:05:07 | 4    |",
+            "| red   | 18.0  | 1996-04-12T12:05:08 | 5    |",
+            "| red   | 17.0  | 1996-04-12T12:05:09 | 6    |",
+            "| red   | 7.0   | 1996-04-12T12:05:10 | 7    |",
+            "| red   | 7.1   | 1996-04-12T12:05:11 | 8    |",
+            "| red   | 7.2   | 1996-04-12T12:05:12 | 9    |",
+            "| red   | 3.0   | 1996-04-12T12:05:13 | 10   |",
+            "| red   | 1.0   | 1996-04-12T12:05:14 | 11   |",
+            "| red   | 0.0   | 1996-04-12T12:05:15 | 12   |",
+            "+-------+-------+---------------------+------+",
         ],
         &rows.collect().await?
     );
@@ -71,27 +95,47 @@ async fn test_add_int_col_to_df() -> Result<()> {
 #[tokio::test]
 async fn test_add_str_col_to_df() -> Result<()> {
     let ctx = SessionContext::new();
-    let df = dataframe!(
-        "id" => [1, 2, 3],
-        "data" => [42, 43, 44]
-    )?;
+    let cars = ExampleDataset::Cars;
+    let df = cars.dataframe(&ctx).await?;
 
-    let data = vec!["foo", "bar", "baz"];
+    let data = vec!["foo"; 25];
     let res = add_str_col_to_df(&ctx, df, data, "name").await?;
 
-    assert_eq!(res.schema().fields().len(), 3); // columns count
-    assert_eq!(res.clone().count().await?, 3); // rows count
+    assert_eq!(res.schema().fields().len(), 4); // columns count
+    assert_eq!(res.clone().count().await?, 25); // rows count
 
-    let rows = res.sort(vec![col("id").sort(true, true)])?;
+    let rows = res.sort(vec![col("car").sort(true, true)])?;
     assert_batches_eq!(
         &[
-            "+----+------+------+",
-            "| id | data | name |",
-            "+----+------+------+",
-            "| 1  | 42   | foo  |",
-            "| 2  | 43   | bar  |",
-            "| 3  | 44   | baz  |",
-            "+----+------+------+",
+            "+-------+-------+---------------------+------+",
+            "| car   | speed | time                | name |",
+            "+-------+-------+---------------------+------+",
+            "| green | 10.0  | 1996-04-12T12:05:03 | foo  |",
+            "| green | 10.3  | 1996-04-12T12:05:04 | foo  |",
+            "| green | 10.4  | 1996-04-12T12:05:05 | foo  |",
+            "| green | 10.5  | 1996-04-12T12:05:06 | foo  |",
+            "| green | 11.0  | 1996-04-12T12:05:07 | foo  |",
+            "| green | 12.0  | 1996-04-12T12:05:08 | foo  |",
+            "| green | 14.0  | 1996-04-12T12:05:09 | foo  |",
+            "| green | 15.0  | 1996-04-12T12:05:10 | foo  |",
+            "| green | 15.1  | 1996-04-12T12:05:11 | foo  |",
+            "| green | 15.2  | 1996-04-12T12:05:12 | foo  |",
+            "| green | 8.0   | 1996-04-12T12:05:13 | foo  |",
+            "| green | 2.0   | 1996-04-12T12:05:14 | foo  |",
+            "| red   | 20.0  | 1996-04-12T12:05:03 | foo  |",
+            "| red   | 20.3  | 1996-04-12T12:05:04 | foo  |",
+            "| red   | 21.4  | 1996-04-12T12:05:05 | foo  |",
+            "| red   | 21.5  | 1996-04-12T12:05:06 | foo  |",
+            "| red   | 19.0  | 1996-04-12T12:05:07 | foo  |",
+            "| red   | 18.0  | 1996-04-12T12:05:08 | foo  |",
+            "| red   | 17.0  | 1996-04-12T12:05:09 | foo  |",
+            "| red   | 7.0   | 1996-04-12T12:05:10 | foo  |",
+            "| red   | 7.1   | 1996-04-12T12:05:11 | foo  |",
+            "| red   | 7.2   | 1996-04-12T12:05:12 | foo  |",
+            "| red   | 3.0   | 1996-04-12T12:05:13 | foo  |",
+            "| red   | 1.0   | 1996-04-12T12:05:14 | foo  |",
+            "| red   | 0.0   | 1996-04-12T12:05:15 | foo  |",
+            "+-------+-------+---------------------+------+",
         ],
         &rows.collect().await?
     );
@@ -102,32 +146,52 @@ async fn test_add_str_col_to_df() -> Result<()> {
 #[tokio::test]
 async fn test_add_any_num_col_to_df() -> Result<()> {
     let ctx = SessionContext::new();
-    let df = dataframe!(
-        "id" => [1, 2, 3],
-        "data" => [42, 43, 44]
-    )?;
+    let cars = ExampleDataset::Cars;
+    let df = cars.dataframe(&ctx).await?;
 
-    let data = vec![1, 2, 3];
+    let data = (0..25).step_by(1).collect::<Vec<i32>>();
     let data_col = Int32Array::from(data);
     let df = add_any_num_col_to_df(&ctx, df, data_col, "col1").await?;
 
-    let data = vec![1.1, 1.2, 1.3];
+    let data = (0..25).map(|x| x as f64).collect::<Vec<f64>>();
     let data_col = Float64Array::from(data);
     let res = add_any_num_col_to_df(&ctx, df, data_col, "col2").await?;
 
-    assert_eq!(res.schema().fields().len(), 4); // columns count
-    assert_eq!(res.clone().count().await?, 3); // rows count
+    assert_eq!(res.schema().fields().len(), 5); // columns count
+    assert_eq!(res.clone().count().await?, 25); // rows count
 
-    let rows = res.sort(vec![col("id").sort(true, true)])?;
+    let rows = res.sort(vec![col("car").sort(true, true)])?;
     assert_batches_eq!(
         &[
-            "+----+------+------+------+",
-            "| id | data | col1 | col2 |",
-            "+----+------+------+------+",
-            "| 1  | 42   | 1    | 1.1  |",
-            "| 2  | 43   | 2    | 1.2  |",
-            "| 3  | 44   | 3    | 1.3  |",
-            "+----+------+------+------+",
+            "+-------+-------+---------------------+------+------+",
+            "| car   | speed | time                | col1 | col2 |",
+            "+-------+-------+---------------------+------+------+",
+            "| green | 10.0  | 1996-04-12T12:05:03 | 13   | 13.0 |",
+            "| green | 10.3  | 1996-04-12T12:05:04 | 14   | 14.0 |",
+            "| green | 10.4  | 1996-04-12T12:05:05 | 15   | 15.0 |",
+            "| green | 10.5  | 1996-04-12T12:05:06 | 16   | 16.0 |",
+            "| green | 11.0  | 1996-04-12T12:05:07 | 17   | 17.0 |",
+            "| green | 12.0  | 1996-04-12T12:05:08 | 18   | 18.0 |",
+            "| green | 14.0  | 1996-04-12T12:05:09 | 19   | 19.0 |",
+            "| green | 15.0  | 1996-04-12T12:05:10 | 20   | 20.0 |",
+            "| green | 15.1  | 1996-04-12T12:05:11 | 21   | 21.0 |",
+            "| green | 15.2  | 1996-04-12T12:05:12 | 22   | 22.0 |",
+            "| green | 8.0   | 1996-04-12T12:05:13 | 23   | 23.0 |",
+            "| green | 2.0   | 1996-04-12T12:05:14 | 24   | 24.0 |",
+            "| red   | 20.0  | 1996-04-12T12:05:03 | 0    | 0.0  |",
+            "| red   | 20.3  | 1996-04-12T12:05:04 | 1    | 1.0  |",
+            "| red   | 21.4  | 1996-04-12T12:05:05 | 2    | 2.0  |",
+            "| red   | 21.5  | 1996-04-12T12:05:06 | 3    | 3.0  |",
+            "| red   | 19.0  | 1996-04-12T12:05:07 | 4    | 4.0  |",
+            "| red   | 18.0  | 1996-04-12T12:05:08 | 5    | 5.0  |",
+            "| red   | 17.0  | 1996-04-12T12:05:09 | 6    | 6.0  |",
+            "| red   | 7.0   | 1996-04-12T12:05:10 | 7    | 7.0  |",
+            "| red   | 7.1   | 1996-04-12T12:05:11 | 8    | 8.0  |",
+            "| red   | 7.2   | 1996-04-12T12:05:12 | 9    | 9.0  |",
+            "| red   | 3.0   | 1996-04-12T12:05:13 | 10   | 10.0 |",
+            "| red   | 1.0   | 1996-04-12T12:05:14 | 11   | 11.0 |",
+            "| red   | 0.0   | 1996-04-12T12:05:15 | 12   | 12.0 |",
+            "+-------+-------+---------------------+------+------+",
         ],
         &rows.collect().await?
     );
@@ -138,32 +202,52 @@ async fn test_add_any_num_col_to_df() -> Result<()> {
 #[tokio::test]
 async fn test_add_any_str_col_to_df() -> Result<()> {
     let ctx = SessionContext::new();
-    let df = dataframe!(
-        "id" => [1, 2, 3],
-        "data" => [42, 43, 44]
-    )?;
-
-    let data = vec!["foo", "bar", "baz"];
+    let cars = ExampleDataset::Cars;
+    let df = cars.dataframe(&ctx).await?;
+    
+    let data = vec!["foo"; 25];
     let data_col = StringArray::from(data);
     let df = add_any_str_col_to_df(&ctx, df, data_col, "col1").await?;
 
-    let data = vec!["foo", "bar", "baz"];
+    let data = vec!["bar"; 25];
     let data_col = LargeStringArray::from(data);
     let res = add_any_str_col_to_df(&ctx, df, data_col, "col2").await?;
 
-    assert_eq!(res.schema().fields().len(), 4); // columns count
-    assert_eq!(res.clone().count().await?, 3); // rows count
+    assert_eq!(res.schema().fields().len(), 5); // columns count
+    assert_eq!(res.clone().count().await?, 25); // rows count
 
-    let rows = res.sort(vec![col("id").sort(true, true)])?;
+    let rows = res.sort(vec![col("car").sort(true, true)])?;
     assert_batches_eq!(
         &[
-            "+----+------+------+------+",
-            "| id | data | col1 | col2 |",
-            "+----+------+------+------+",
-            "| 1  | 42   | foo  | foo  |",
-            "| 2  | 43   | bar  | bar  |",
-            "| 3  | 44   | baz  | baz  |",
-            "+----+------+------+------+",
+            "+-------+-------+---------------------+------+------+",
+            "| car   | speed | time                | col1 | col2 |",
+            "+-------+-------+---------------------+------+------+",
+            "| green | 10.0  | 1996-04-12T12:05:03 | foo  | bar  |",
+            "| green | 10.3  | 1996-04-12T12:05:04 | foo  | bar  |",
+            "| green | 10.4  | 1996-04-12T12:05:05 | foo  | bar  |",
+            "| green | 10.5  | 1996-04-12T12:05:06 | foo  | bar  |",
+            "| green | 11.0  | 1996-04-12T12:05:07 | foo  | bar  |",
+            "| green | 12.0  | 1996-04-12T12:05:08 | foo  | bar  |",
+            "| green | 14.0  | 1996-04-12T12:05:09 | foo  | bar  |",
+            "| green | 15.0  | 1996-04-12T12:05:10 | foo  | bar  |",
+            "| green | 15.1  | 1996-04-12T12:05:11 | foo  | bar  |",
+            "| green | 15.2  | 1996-04-12T12:05:12 | foo  | bar  |",
+            "| green | 8.0   | 1996-04-12T12:05:13 | foo  | bar  |",
+            "| green | 2.0   | 1996-04-12T12:05:14 | foo  | bar  |",
+            "| red   | 20.0  | 1996-04-12T12:05:03 | foo  | bar  |",
+            "| red   | 20.3  | 1996-04-12T12:05:04 | foo  | bar  |",
+            "| red   | 21.4  | 1996-04-12T12:05:05 | foo  | bar  |",
+            "| red   | 21.5  | 1996-04-12T12:05:06 | foo  | bar  |",
+            "| red   | 19.0  | 1996-04-12T12:05:07 | foo  | bar  |",
+            "| red   | 18.0  | 1996-04-12T12:05:08 | foo  | bar  |",
+            "| red   | 17.0  | 1996-04-12T12:05:09 | foo  | bar  |",
+            "| red   | 7.0   | 1996-04-12T12:05:10 | foo  | bar  |",
+            "| red   | 7.1   | 1996-04-12T12:05:11 | foo  | bar  |",
+            "| red   | 7.2   | 1996-04-12T12:05:12 | foo  | bar  |",
+            "| red   | 3.0   | 1996-04-12T12:05:13 | foo  | bar  |",
+            "| red   | 1.0   | 1996-04-12T12:05:14 | foo  | bar  |",
+            "| red   | 0.0   | 1996-04-12T12:05:15 | foo  | bar  |",
+            "+-------+-------+---------------------+------+------+",
         ],
         &rows.collect().await?
     );
@@ -174,26 +258,48 @@ async fn test_add_any_str_col_to_df() -> Result<()> {
 #[tokio::test]
 async fn test_add_col_arr_to_df() -> Result<()> {
     let ctx = SessionContext::new();
-    let df = dataframe!(
-        "id" => [1, 2, 3],
-        "data" => [42, 43, 44]
-    )?;
+    let cars = ExampleDataset::Cars;
+    let df = cars.dataframe(&ctx).await?;
 
-    let col1 = StringArray::from(vec!["foo", "bar", "baz"]);
-    let col2 = Float64Array::from(vec![42.0, 43.0, 44.0]);
+    let data = (0..25).step_by(1).collect::<Vec<i32>>();
+    let col1 = Int32Array::from(data);
+    let data = vec!["foo"; 25];
+    let col2 = StringArray::from(data);
     let df = add_col_arr_to_df(&ctx, df, &col1, "col1").await?;
     let res = add_col_arr_to_df(&ctx, df, &col2, "col2").await?;
 
-    let rows = res.sort(vec![col("id").sort(true, true)])?;
+    let rows = res.sort(vec![col("car").sort(true, true)])?;
     assert_batches_eq!(
         &[
-            "+----+------+------+------+",
-            "| id | data | col1 | col2 |",
-            "+----+------+------+------+",
-            "| 1  | 42   | foo  | 42.0 |",
-            "| 2  | 43   | bar  | 43.0 |",
-            "| 3  | 44   | baz  | 44.0 |",
-            "+----+------+------+------+",
+            "+-------+-------+---------------------+------+------+",
+            "| car   | speed | time                | col1 | col2 |",
+            "+-------+-------+---------------------+------+------+",
+            "| green | 10.0  | 1996-04-12T12:05:03 | 13   | foo  |",
+            "| green | 10.3  | 1996-04-12T12:05:04 | 14   | foo  |",
+            "| green | 10.4  | 1996-04-12T12:05:05 | 15   | foo  |",
+            "| green | 10.5  | 1996-04-12T12:05:06 | 16   | foo  |",
+            "| green | 11.0  | 1996-04-12T12:05:07 | 17   | foo  |",
+            "| green | 12.0  | 1996-04-12T12:05:08 | 18   | foo  |",
+            "| green | 14.0  | 1996-04-12T12:05:09 | 19   | foo  |",
+            "| green | 15.0  | 1996-04-12T12:05:10 | 20   | foo  |",
+            "| green | 15.1  | 1996-04-12T12:05:11 | 21   | foo  |",
+            "| green | 15.2  | 1996-04-12T12:05:12 | 22   | foo  |",
+            "| green | 8.0   | 1996-04-12T12:05:13 | 23   | foo  |",
+            "| green | 2.0   | 1996-04-12T12:05:14 | 24   | foo  |",
+            "| red   | 20.0  | 1996-04-12T12:05:03 | 0    | foo  |",
+            "| red   | 20.3  | 1996-04-12T12:05:04 | 1    | foo  |",
+            "| red   | 21.4  | 1996-04-12T12:05:05 | 2    | foo  |",
+            "| red   | 21.5  | 1996-04-12T12:05:06 | 3    | foo  |",
+            "| red   | 19.0  | 1996-04-12T12:05:07 | 4    | foo  |",
+            "| red   | 18.0  | 1996-04-12T12:05:08 | 5    | foo  |",
+            "| red   | 17.0  | 1996-04-12T12:05:09 | 6    | foo  |",
+            "| red   | 7.0   | 1996-04-12T12:05:10 | 7    | foo  |",
+            "| red   | 7.1   | 1996-04-12T12:05:11 | 8    | foo  |",
+            "| red   | 7.2   | 1996-04-12T12:05:12 | 9    | foo  |",
+            "| red   | 3.0   | 1996-04-12T12:05:13 | 10   | foo  |",
+            "| red   | 1.0   | 1996-04-12T12:05:14 | 11   | foo  |",
+            "| red   | 0.0   | 1996-04-12T12:05:15 | 12   | foo  |",
+            "+-------+-------+---------------------+------+------+",
         ],
         &rows.collect().await?
     );
